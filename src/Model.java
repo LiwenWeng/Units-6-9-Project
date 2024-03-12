0fcbecd1cb139a9064a1f59973e08038d5d86847
BT_1N3_01 <sample@bths.edu>
2024-03-12 10:38:32 -0400

    added code to model

--- src/Model.java
+++ src/Model.java
@@ -1,3 +1,10 @@
 public class Model {
-
+    private String symbol;
+    private String name;
+    private Vector2 vector;
+    public Model(String symbol, String name, int x, int y) {
+        this.vector = new Vector2(x, y);
+        this.symbol = symbol;
+        this.name = name;
+    }
 }

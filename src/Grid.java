9e57add78f114713471ab6edd5903eae888c4151
BT_1N3_02 <sample@bths.edu>
2024-03-12 10:40:33 -0400

    Added stuff to Model

--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,10 @@
 public class Grid {
+    private Model[][] map;
+
+    public Grid() {
+        map = new Model[5][8];
+
+    }
+
 
 }
--- src/Model.java
+++ src/Model.java
@@ -1,10 +1,17 @@
 public class Model {
     private String symbol;
     private String name;
-    private Vector2 vector;
-    public Model(String symbol, String name, int x, int y) {
-        this.vector = new Vector2(x, y);
+    private Vector2 position;
+    private int renderPriority;
+
+    public Model(String name, String symbol, Vector2 position, int renderPriority) {
+        this.position = position;
         this.symbol = symbol;
         this.name = name;
+        this.renderPriority = renderPriority;
+    }
+
+    public Model(Vector2 position) {
+        this("Grass", Utils.color("_", "Green"), position, 0);
     }
 }

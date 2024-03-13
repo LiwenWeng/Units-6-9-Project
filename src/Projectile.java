a7b09eec59eede978580ab29aae6ee66c1e28822
BT_1N3_01 <sample@bths.edu>
2024-03-13 10:34:08 -0400

    added to projectile

--- src/Projectile.java
+++ src/Projectile.java
@@ -1,4 +1,9 @@
-public class Projectile  {
+public class Projectile extends Model {
     int damage;
+    public Projectile(String name, String symbol, Vector2 position, int renderPriority, int damage) {
+        super(name, symbol, position, renderPriority);
+        this.damage = damage;
+    }
+
 
 }

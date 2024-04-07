d079375410653882672e1481c06d0861247b688c
ChongOscar <ohlookoscarshere@gmail.com>
2024-04-07 19:48:35 -0400

    fixed lawnmower killing

--- src/Models/Lawnmower.java
+++ src/Models/Lawnmower.java
@@ -9,13 +9,14 @@ public class Lawnmower extends Model {
                 Model model = getGrid().contains(Zombie.class, new Vector2(getPosition().getX(), getPosition().getY() + 1));
                 if (model instanceof Zombie) {
                     while (getPosition().getY() < 7) {
+
+                        getGrid().remove(this);
+                        getPosition().setY(getPosition().getY() + 1);
+                        getGrid().place(this);
                         model = getGrid().contains(Zombie.class, new Vector2(getPosition().getX(), getPosition().getY()));
                         if (model instanceof Zombie) {
                             ((Zombie) model).takeDamage(10000);
                         }
-                        getGrid().remove(this);
-                        getPosition().setY(getPosition().getY() + 1);
-                        getGrid().place(this);
                         Utils.wait(300);
                     }
                     getGrid().remove(this);
